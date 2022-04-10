3c387825b2fa3a56a9210dd450a350b694ce7ff6
saintabomination <saint.abomination@protonmail.com>
2022-04-10 14:36:19 +0200

    Adding SFML to the project

--- bake.sh
+++ bake.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 PROJECT_NAME="sfml-game"
+LIBRARIES="-lsfml-graphics -lsfml-window -lsfml-system"
 
 g++ -c src/Main.cpp
-g++ *.o -o build/$PROJECT_NAME
+g++ *.o -o build/$PROJECT_NAME $LIBRARIES
 rm *.o
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,34 @@
-#include "iostream"
+#include "Headers.h"
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
 
 int main()
 {
-  std::cout << "Hello, World!" << std::endl;
+  sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML");
+  sf::CircleShape shape(20.f);
+  shape.setFillColor(sf::Color::Red);
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      switch (event.type)
+      {
+        case sf::Event::Closed:
+          window.close();
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
+
   return 0;
 }
